2dca845cfd4b0d9067c885f44104f96ca17b635e
hulateacher <kahalahula@gmail.com>
2015-12-12 15:26:34 -1000

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    url(r'', include('hula.urls')),
 ]
 
    
